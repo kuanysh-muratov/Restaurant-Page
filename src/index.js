3f606b46c895ded3f8f90c749eeb5350be085279
Kuanysh Muratov <kuanysh.muratov.05@gmail.com>
2024-09-22 13:21:55 +0900

    Commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,79 @@
 import "./style.css";
 import Dripping from "./dripping.png";
-import {createHomePage} from "./home.js";
-import {createContactPage} from "./contact.js";
-import {createMenuPage} from "./menu.js";
-const buttons=document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      document.querySelectorAll(".dripping").forEach(img => img.remove());
-      document.querySelectorAll(".pressed").forEach(pressed=> pressed.classList.remove("pressed"));
-      document.querySelector("#content").innerHTML = "";
-      const myImg = new Image();
-      myImg.src = Dripping; 
-      myImg.classList.add("dripping");
-      button.classList.add("pressed");
-      button.appendChild(myImg);
-      switch(button.getAttribute("id")){
-        case "Home":
-            createHomePage();
-            break;
-        case "Menu":
-            createMenuPage();
-            break;
-        case "Contact":
-            createContactPage();
-            break;
-      }
+import { createHomePage } from "./home.js";
+import { createContactPage } from "./contact.js";
+import { createMenuPage } from "./menu.js";
+
+class RestaurantApp {
+  constructor() {
+    this.contentElement = document.querySelector("#content");
+    this.buttons = document.querySelectorAll("button");
+    this.currentPage = null;
+    this.initializeEventListeners();
+    this.navigateTo("Home");
+    this.applyInitialHomeButtonEffect();
+  }
+
+  initializeEventListeners() {
+    this.buttons.forEach(button => {
+      button.addEventListener("click", () => this.handleNavigation(button));
     });
-  });
-  document.querySelector("#Home").click();
+  }
+
+  handleNavigation(button) {
+    const pageId = button.getAttribute("id");
+    this.clearCurrentPage();
+    this.updateButtonStyles(button);
+    this.navigateTo(pageId);
+  }
+
+  clearCurrentPage() {
+    this.removeAllDrippingImages();
+    this.removeAllPressedStyles();
+    this.contentElement.innerHTML = "";
+  }
+
+  removeAllDrippingImages() {
+    document.querySelectorAll(".dripping").forEach(img => img.remove());
+  }
+
+  removeAllPressedStyles() {
+    document.querySelectorAll(".pressed").forEach(element => element.classList.remove("pressed"));
+  }
+
+  updateButtonStyles(button) {
+    const drippingImage = new Image();
+    drippingImage.src = Dripping;
+    drippingImage.classList.add("dripping");
+    button.classList.add("pressed");
+    button.appendChild(drippingImage);
+  }
+
+  navigateTo(pageId) {
+    switch(pageId) {
+      case "Home":
+        this.currentPage = createHomePage();
+        break;
+      case "Menu":
+        this.currentPage = createMenuPage();
+        break;
+      case "Contact":
+        this.currentPage = createContactPage();
+        break;
+      default:
+        console.error(`Unknown page: ${pageId}`);
+    }
+  }
+
+  applyInitialHomeButtonEffect() {
+    const homeButton = document.querySelector("#Home");
+    if (homeButton) {
+      this.updateButtonStyles(homeButton);
+    }
+  }
+}
+
+// Initialize the app
+document.addEventListener("DOMContentLoaded", () => {
+  new RestaurantApp();
+});
